2582612f1f6011cc02daf762848c29086c54d103
characTooling <90096569+characTooling@users.noreply.github.com>
2024-07-12 20:49:33 +0700

    fix cookies

--- cookies.js
+++ cookies.js
@@ -1,45 +1,42 @@
-
 function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  
-  function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Lax";
+  console.log(`Setting cookie: ${cname}=${cvalue}; ${expires}; path=/`);
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i].trim();
+    if (c.indexOf(name) == 0) {
+      console.log(`Getting cookie: ${cname}=${c.substring(name.length, c.length)}`);
+      return c.substring(name.length, c.length);
     }
-    return "";
   }
-  function addZero(i) {
-    if (i < 10) {i = "0" + i}
-    return i;
+  console.log(`Cookie ${cname} not found`);
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user) {
+    let time = new Date(parseInt(user));
+    alert("Welcome again. Your last visit was at: " + time);
+    console.log("User found, updating visit time");
+    let date = new Date();
+    let timeTrack = date.getTime();
+    setCookie("username", timeTrack.toString(), 365);
+  } else {
+    alert("Welcome to my homepage");
+    console.log("No user found, setting new cookie");
+    let date = new Date();
+    let timeTrack = date.getTime();
+    setCookie("username", timeTrack.toString(), 365);
   }
-  function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-      let time = new Date(parseInt(user));
-      alert("Welcome again. Your last visit was at: " + time);
-      let date = new Date()
-      let timeTrack = date.getTime();
-      if (timeTrack != null) {
-        setCookie("username", timeTrack.toString(), 365);
-      }
-    } else {
-      alert("Welcome to my homepage")
-      let date = new Date()
-      user = date.getTime();
-      if (user != null) {
-        setCookie("username", user.toString(), 365);
-      }
-    }
-  } 
+}
+
+window.onload = checkCookie;
