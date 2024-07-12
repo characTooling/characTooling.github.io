ef68774cd6275ce77053cd0fea89c01d28f1d8bf
characTooling <90096569+characTooling@users.noreply.github.com>
2024-07-12 21:23:02 +0700

    Add files via upload

--- etsep.js
+++ etsep.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var date1, date2;  
-    // Define two date object variables with dates inside it  
     date1 = new Date("09/01/1993");
     function addZero(i) {
       if (i < 10) {i = "0" + i}
       return i;
     }
-    
     function eternal_september() {
       date2 = new Date();  
-      // Calculate time difference  
       var time_difference = date2.getTime() - date1.getTime();  
       var timezone = -date2.getTimezoneOffset() / 60;
       var sign = "+";
@@ -18,11 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       let h = addZero(date2.getHours());
       let m = addZero(date2.getMinutes());
-      let s = addZero(date2.getSeconds());
-      // Calculate days difference by dividing total milliseconds in a day and rounding the result  
+      let s = addZero(date2.getSeconds()); 
       var days_difference = Math.floor(time_difference / (1000 * 60 * 60 * 24)) + 1;  
-      
-      // Determine the correct suffix for the day
       var suffix = "th";
       if (days_difference % 10 == 1 && days_difference % 100 != 11) {
         suffix = "st";
@@ -31,11 +25,9 @@ document.addEventListener("DOMContentLoaded", function() {
       } else if (days_difference % 10 == 3 && days_difference % 100 != 13) {
         suffix = "rd";
       }
-
       document.getElementById("etsep").innerHTML = "It is currently " + h + ":" + m + ":" + s + " GMT" + sign + timezone + ", " + days_difference + suffix + " September, 1993. Welcome to the September that never ends. Welcome to the Internet.";  
     }
 
-    // Call the function immediately to avoid initial delay
     eternal_september();
     setInterval(eternal_september, 500);
   });
